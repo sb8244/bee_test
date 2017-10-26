db743de25642aad83c2e6b44aabd3e8e13b20604
Stephen Bussey <sb8244@cs.ship.edu>
2017-10-26 19:30:21 -0400

    Include content type whitelisting

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -4,4 +4,8 @@ class PhotoUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
+
+  def content_type_whitelist
+    /image\//
+  end
 end
