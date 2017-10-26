a9c2c55248bb923623923f30fc45284669c0320b
Stephen Bussey <sb8244@cs.ship.edu>
2017-10-26 18:39:09 -0400

    Use a token for the JWT password

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   def self.load_from_token(token)
-    payload = JWT.decode(token, nil, false)[0]
+    payload = JWT.decode(token, ENV.fetch("USER_PAYLOAD_TOKEN"), false)[0]
     self.find(payload.fetch("id"))
   rescue JWT::DecodeError
     raise ActiveRecord::RecordNotFound
   end
 
   def get_auth_token
-    JWT.encode({ id: self.id }, nil, 'none')
+    JWT.encode({ id: self.id }, ENV.fetch("USER_PAYLOAD_TOKEN"), 'none')
   end
 end
