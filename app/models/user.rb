0a5f4c6535b6ab29402e041f039bc96120a667eb
Stephen Bussey <sb8244@cs.ship.edu>
2017-10-26 18:42:49 -0400

    Note about JWT

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  # NOTE: Generally, you should avoid ENV based JWT and prefer pem signed tokens if possible
   def self.load_from_token(token)
     payload = JWT.decode(token, ENV.fetch("USER_PAYLOAD_TOKEN"), false)[0]
     self.find(payload.fetch("id"))
